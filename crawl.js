b3bb3d873df6d3466ff3d038f81b0542bb0c0dfb
Gosia <marnadusza@gmail.com>
2024-04-13 17:22:05 +0200

    crawl single page with errors handling

--- crawl.js
+++ crawl.js
@@ -1,6 +1,34 @@
 const jsdom = require('jsdom')
 const { JSDOM } = jsdom
 
+async function crawlPage(currentURL) {
+	console.log(`actively crawling: ${currentURL}`)
+	try {
+	const resp = await fetch(currentURL)
+	
+	if(resp.status > 399) {
+		console.log(`error in fetch with status code: ${resp.status} on page ${currentURL}`)
+		
+		return
+	}
+
+	const contentType = resp.headers.get('Content-Type')
+	if(!contentType.includes('text/html')){
+	console.log(`no html response: content-type: ${contentType} on page ${currentURL}`)
+	return
+	}
+		
+	console.log(await resp.text())
+
+	} catch(err) {
+		console.log(`error in fetch: ${err.massage}, on page ${currentURL}`)
+	}
+	
+
+	// return resp.json()
+}
+
+
 function getURLsFromHTML(inputHTMLBody, inputBaseURL) {
 	const urls = []
 	const dom = new JSDOM(inputHTMLBody)
@@ -10,24 +38,21 @@ function getURLsFromHTML(inputHTMLBody, inputBaseURL) {
 		if (link.href.startsWith('/')) {
 			try {
 				const urlObj = new URL(`${inputBaseURL}${link.href}`)
-				urls.push(urlObj.href)}
-				catch (err) {
-					console.log(`error with relative url: ${err.message}`)
-				}
-		} else  {
+				urls.push(urlObj.href)
+			} catch (err) {
+				console.log(`error with relative url: ${err.message}`)
+			}
+		} else {
 			try {
 				const urlObj = new URL(`${link.href}`)
-				urls.push(urlObj.href)}
-				catch (err) {
-					console.log(`error with absolute url: ${err.message}`)
-				}
-			
-		} 
+				urls.push(urlObj.href)
+			} catch (err) {
+				console.log(`error with absolute url: ${err.message}`)
+			}
+		}
 	}
 	return urls
 }
-
-
 
 function normalizeURL(urlString) {
 	const urlObj = new URL(urlString)
@@ -43,4 +68,5 @@ function normalizeURL(urlString) {
 module.exports = {
 	normalizeURL,
 	getURLsFromHTML,
+	crawlPage
 }
